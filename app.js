006cd8f222b3bb3f9c09c37ba770ffafc43c8c0b
Peter Manasseh Oz <chibuezeozojideofor@gmail.com>
2024-02-04 19:30:35 +0100

    Make the cart quantity interactive

--- app.js
+++ app.js
@@ -115,7 +115,14 @@ if (matchingItem) {
     quantity: 1
    });
 }
-   console.log(cart);
+
+let cartQuantity = 0;
+
+cart.forEach((item) => {
+cartQuantity += item.quantity;
+});
+
+document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
   });
 });
 
